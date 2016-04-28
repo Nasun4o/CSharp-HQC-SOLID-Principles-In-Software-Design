1e2660e01d99a7429d554227cdf295072859f9cc
nasun4o <nasunawow@abv.bg>
2016-04-28 15:13:01 +0300

    Homework

---
+++ ConsoleAppTest/CrazyFormatter.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppTest
+{
+    using System;
+    using SOLIDLogger;
+    using SOLIDLogger.Interfaces;
+
+    public class CrazyFormatter : IFormatter
+    {
+        public string Format(string msg, ReportLevel level, DateTime date)
+        {
+            return $"{msg} <<{date}>> ??{level}??";
+        }
+    }
+}
